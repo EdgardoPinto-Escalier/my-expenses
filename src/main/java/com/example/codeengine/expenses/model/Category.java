4e9014c49a07a10e3f643c369bdabc622c72efca
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-04-29 08:28:47 +0200

    Created the Category class

--- src/main/java/com/example/codeengine/expenses/model/Category.java
+++ src/main/java/com/example/codeengine/expenses/model/Category.java
@@ -1,17 +1,20 @@
 package com.example.codeengine.expenses.model;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
+@Data
 @Table(name="category")
 public class Category {
 
     @Id
     private Long id;
     private String name;
+
+    @ManyToOne(cascade= CascadeType.PERSIST)
+    private User user;
 }
--- src/main/java/com/example/codeengine/expenses/model/Expenses.java
+++ src/main/java/com/example/codeengine/expenses/model/Expenses.java
@@ -6,22 +6,26 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import java.util.Set;
+import java.time.Instant;
 
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Data
-@Table(name="user")
-public class User {
+@Table(name="expenses")
+public class Expenses {
 
     @Id
     private Long id;
-    private String name;
-    private String email;
 
-    @OneToMany
-    private Set<Category> category;
+    private Instant expensedate;
+    private String description;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private User user;
 }
